2f7191c72a50f738fe09063904b437f52f294fb0
markdev <markpavlenkos@gmail.com>
2024-05-02 08:29:53 +0300

    custom boot anim

---
+++ boot/boot.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Print ASCII art logo during boot
+echo "
+   PPPP   y   y  TTTTT  EEEE  RRRR  M   M 
+   P   P   y y     T    E     R   R MM MM       
+   PPPP     y      T    EEE   RRRR  M M M       
+   P        y      T    E     R  R  M   M      
+   P        y      T    EEEE  R   R M   M 
+                           Operating System
+"
+# Continue with other startup commands
