09c5d5581795c32bf428125dcfbb6cac574c8f87
markdev <markpavlenkos@gmail.com>
2024-05-13 17:20:44 +0300

    argh again

--- PyTermOS-Universal/PTCLI/venv копія/lib/python3.12/site-packages/seqeval/metrics/__init__.py
+++
@@ -1,4 +0,0 @@
-from seqeval.metrics.sequence_labeling import (accuracy_score,
-                                               classification_report, f1_score,
-                                               performance_measure,
-                                               precision_score, recall_score)
--- PyTermOS-Universal/PTCLI/venv копія/lib/python3.12/site-packages/simpletransformers/pretrain_retrieval/__init__.py
+++
@@ -1,4 +0,0 @@
-from simpletransformers.config.model_args import RetrievalArgs
-from simpletransformers.pretrain_retrieval.pretrain_retrieval_model import (
-    PretrainRetrievalModel,
-)
